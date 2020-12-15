a25c306eb0479bc23494cffbc175c23a01fa7cdf
Jesus octavio acosta santos <jesusoctavioas@gmail.com>
2020-12-14 20:55:56 -0700

    ammend commit

---
+++ IAmPoor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IAmPoor
+//
+//  Created by Jesus Octavio Acosta Santos on 06/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
